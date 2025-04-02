3d71c45f65551d935590db528d0e60a100a0009c
sdegutis <sbdegutis@gmail.com>
2025-04-02 09:54:47 -0500

    Add hot reloading.

--- main.ts
+++ main.ts
@@ -21,8 +21,12 @@ const twoslashStyle = readFileSync('node_modules/@shikijs/twoslash/style-rich.cs
 
 const tree = new immaculata.LiveTree('site', import.meta.url)
 
+let reloader = ''
+
 if (process.argv[2] === 'dev') {
-  const server = new immaculata.DevServer(8080)
+  reloader = `<script>new EventSource('/reload').onmessage = () => location.reload()</script>`
+
+  const server = new immaculata.DevServer(8080, '/reload')
   server.files = await processSite()
 
   tree.watch({
@@ -44,9 +48,9 @@ async function processSite() {
     files.add("/twoslash.css", twoslashStyle)
 
     const content = files.with('^/rationale.md$').all()[0]?.text!
-    files.del('/rationale.md')
+    files.del('^/rationale.md$')
 
-    files.with('^/index.html$').do(f => f.text = f.text.replace('<!-- CONTENT -->', md.render(content)))
+    files.with('^/index.html$').do(f => f.text = f.text.replace('<!-- CONTENT -->', reloader + md.render(content)))
 
   })
 }
