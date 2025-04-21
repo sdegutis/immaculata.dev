be2b46ec9828e1736ff30f856f93f475da462980
sdegutis <sbdegutis@gmail.com>
2025-04-20 21:55:39 -0500

    Remove twoslash.

--- site/build.ts
+++ site/build.ts
@@ -1,9 +1,6 @@
-import { readFileSync } from "fs"
 import * as immaculata from 'immaculata'
 import { md } from "./highlight.ts"
 import { mainPage } from "./index.html.tsx"
-
-const twoslashStyle = readFileSync('node_modules/@shikijs/twoslash/style-rich.css')
 
 let reloader = ''
 if (process.argv[2] === 'dev') reloader = `<script>new EventSource('/reload').onmessage = () => location.reload()</script>`
@@ -13,8 +10,6 @@ export async function processSite(tree: immaculata.LiveTree) {
 
     files.without('/public/').remove()
     files.do(f => f.path = f.path.slice('/public'.length))
-
-    files.add("/twoslash.css", twoslashStyle)
 
     const content = tree.files.get('/rationale.md')!.content.toString()
     files.add('/index.html', mainPage(reloader + md.render(content)))
--- site/index.html.tsx
+++ site/index.html.tsx
@@ -9,7 +9,6 @@ export function mainPage(content: string) {
         <meta name="color-scheme" content="light dark" />
         <meta charset="UTF-8" />
         <link rel="stylesheet" href="/style.css" />
-        <link rel="stylesheet" href="/twoslash.css" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>Immaculata.dev</title>
       </head>
