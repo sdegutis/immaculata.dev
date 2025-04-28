fa423e787b3e237112b1d64226ba2276d5684be5
sdegutis <sbdegutis@gmail.com>
2025-04-28 13:00:42 -0500

    JSX type assistance.

--- jsx.d.ts
+++ jsx.d.ts
@@ -1,6 +1,16 @@
 declare namespace JSX {
-  type IntrinsicElements = Record<string, any>
-  interface ElementChildrenAttribute {
-    children: any
+  type IntrinsicElements = {
+    [K in keyof HTMLElementTagNameMap]: {
+      [A in keyof HTMLElementTagNameMap[K]as A extends string ? Lowercase<Exclude<A, 'children'>> : never]?:
+      HTMLElementTagNameMap[K][A] extends (string | boolean | null | number) ? HTMLElementTagNameMap[K][A] | string : string
+    } & {
+      children?: any
+      class?: string
+    }
   }
+  type ElementChildrenAttribute = { children: any }
+  type Element = string
+  type ElementType =
+    | string
+    | ((data: any) => JSX.Element)
 }
--- site/template/core.tsx
+++ site/template/core.tsx
@@ -14,7 +14,7 @@ export function Head() {
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Immaculata.dev</title>
     <link rel="preconnect" href="https://fonts.googleapis.com" />
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin='' />
     <link href="https://fonts.googleapis.com/css2?family=Gemunu+Libre:wght@200..800&family=Martel:wght@200;300;400;600;700;800;900&family=Oxanium:wght@200..800&family=Silkscreen:wght@400;700&display=swap" rel="stylesheet" />
     <script type="module" src="/script/nav.js"></script>
     <script type="module" src="/script/toc.js"></script>
