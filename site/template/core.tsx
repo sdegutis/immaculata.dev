655dc7204401b0b2d2cafc3dd1bed0ba4c35bb59
sdegutis <sbdegutis@gmail.com>
2025-04-29 15:36:13 -0500

    Rename link.

--- site/template/core.tsx
+++ site/template/core.tsx
@@ -51,7 +51,7 @@ export function Navbar(data: {
   const pages = [
     ...data.pages,
     { section: 'links', title: 'Github project', path: 'https://github.com/thesoftwarephilosopher/immaculata' },
-    { section: 'links', title: 'Fork site template', path: 'https://github.com/thesoftwarephilosopher/immaculata.dev' },
+    { section: 'links', title: 'Site template', path: 'https://github.com/thesoftwarephilosopher/immaculata.dev' },
   ]
 
   const groups = Map.groupBy(pages, p => p.section)
