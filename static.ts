97b6aa5b95fea2948fe53c26bb82282b17618735
sdegutis <sbdegutis@gmail.com>
2025-05-03 07:23:57 -0500

    Only vendor font files in fonts.

--- static.ts
+++ static.ts
@@ -1,11 +1,12 @@
 import { readFileSync } from 'fs'
-import { FileTree } from 'immaculata'
+import { FileTree, type ShouldExcludeFile } from 'immaculata'
 
 export const isDev = process.argv[2] === 'dev'
 export const tree = new FileTree('site', import.meta.url)
 
-export const exo2 = new FileTree('node_modules/@fontsource-variable/exo-2', import.meta.url)
-export const monda = new FileTree('node_modules/@fontsource-variable/monda', import.meta.url)
-export const firacode = new FileTree('node_modules/@fontsource-variable/fira-code', import.meta.url)
+const fontExclude: ShouldExcludeFile = (path) => !(path.endsWith('/') || path.match(/\.(css|woff2)$/))
+export const exo2 = new FileTree('node_modules/@fontsource-variable/exo-2', import.meta.url, { exclude: fontExclude })
+export const monda = new FileTree('node_modules/@fontsource-variable/monda', import.meta.url, { exclude: fontExclude })
+export const firacode = new FileTree('node_modules/@fontsource-variable/fira-code', import.meta.url, { exclude: fontExclude })
 
 export const license = readFileSync('node_modules/immaculata/LICENSE', 'utf8')
