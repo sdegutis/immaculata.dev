278b5300a5a847bf5b622bb3fc9c0d0c8d42947f
sdegutis <sbdegutis@gmail.com>
2025-05-06 12:16:37 -0500

    Faster shiki.

--- site/build/highlighter.ts
+++ site/build/highlighter.ts
@@ -3,20 +3,15 @@ import type MarkdownIt from 'markdown-it'
 import * as Shiki from 'shiki'
 import { tree } from '../../static.ts'
 
-// In it's own file since it takes forever to load
-
-console.log('Creating shiki highlighter')
 const highlighter = await Shiki.createHighlighter({
   themes: ['dark-plus'],
-  langs: Object.keys(Shiki.bundledLanguages),
+  langs: ['tsx', 'typescript', 'json', 'yaml', 'bash'],
 })
-console.log('Done creating shiki highlighter')
 
 export function highlightCode(md: MarkdownIt) {
   md.use(ShikiMd.fromHighlighter(highlighter, { theme: 'dark-plus' }))
 }
 
 tree.onModuleInvalidated(import.meta.url, () => {
-  console.log('Disposing shiki highlighter')
   highlighter.dispose()
 })
